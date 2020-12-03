617b0819289e85acb8b9f90d6c6eb0e5cc9512b5
ryandougc <ryan.carr@emailkpu.ca>
2020-12-03 01:20:43 -0800

    removed excess class

--- models/textToSpeech.class.js
+++
@@ -1,9 +0,0 @@
-const fs                        = require('fs');            // Node Filesystem
-const Gtts                      = require('gtts');          // Google text-to-speech
-
-const tmpPath                   = "./tmp/";                                                 // Audio file temporary folder location
-const audioFileName             = Math.floor(Math.random() * 1000000000).toString();        // Audio file random, unique-ish, naming scheme
-
-module.exports = class {
-
-}
